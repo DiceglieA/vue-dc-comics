dfe80b2b07c06009d2b4b2bc0f8cfe28f7ebdc79
DiceglieA <angelodiceglie10@gmail.com>
2022-11-24 15:50:50 +0100

    struttura base vue

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <HeaderComp />
+    <CentralComp />
+    <FooterComp />
+  </div>
+
+</template>
+
+<script>
+import HeaderComp from '@/components/HeaderComp.vue';
+import CentralComp from '@/components/CentralComp.vue';
+import FooterComp from '@/components/FooterComp.vue';
+export default {
+
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+ul {
+  list-style-type: none;
+}
+</style>
---
+++ src/components/CentralComp.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'CentralComp'
+}
+</script>
+
+<style>
+
+</style>
