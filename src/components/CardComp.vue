163336ab9d0321b85aec285ede59484559f83ab0
DiceglieA <angelodiceglie10@gmail.com>
2022-11-25 16:54:00 +0100

    dinamic content

---
+++ src/components/CardComp.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="card">
+    <img :src="card.thumb" alt="">
+    <div>{{card.series}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardComp',
+  props: {
+    card: Object,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.card {
+  width: calc((100% - 5rem) / 6);
+}
+
+img{
+      width: 100%;
+      aspect-ratio: 1/1;
+      object-fit:cover;
+      object-position: top;
+   }</style>
